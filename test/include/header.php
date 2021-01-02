6e46a149a4d215a50eaf15513fd8ef1e199f194b
Bill Nash <wrnash1@gmail.com>
2021-01-02 14:49:50 -0600

    added bootstrap

--- test/include/header.php
+++ test/include/header.php
@@ -1,6 +1,12 @@
 <!-- HEADER DIV START -->
-<div id="header" class="jumbotron">
-  <img id="logo1" src="https://txwes.edu/media/communications-office/brand-toolbox/txwes-logos/TxWesPrimaryLogo_onWhite.png" alt="logo">
-  <img id="logo2" src="static/eyh.png" alt="logo">
+<div id="header" class="container-fluid">
+  <div class="row">
+    <div class="col-10">
+      <img id="logo1" src="https://txwes.edu/media/communications-office/brand-toolbox/txwes-logos/TxWesPrimaryLogo_onWhite.png" alt="logo">
+    </div>
+    <div class="col-2">
+      <img id="logo2" src="static/eyh.png" alt="logo">
+    </div>
+  </div>
 </div>
-<!-- HEADER DIV END -->
+  <!-- HEADER DIV END -->
--- test/include/limit.php
+++ test/include/limit.php
@@ -1,4 +1,5 @@
 <!-- MAIN DIV START -->
+
 <div id="main-content">
   <h1>Student Registration</h1>
   <p>Space is limited, so register early! Registration confirmation will be emailed to your email address as soon as we've processed your information.</p>
