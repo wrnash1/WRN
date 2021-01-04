9ae4017d1ddbfa1e0ce3c7d0808f9808ebe9f855
Bill Nash <wrnash1@gmail.com>
2021-01-04 13:19:22 -0600

    footer updated

--- test/include/footer.php
+++ test/include/footer.php
@@ -1,16 +1,20 @@
 <!-- begin footer -->
 <p class="text-center">**Subject to Availability</p>
-          <div class="text-center">
-            <span>Make checks payable to <u>Expanding Your Horizons</u>.</span><br>
-            <span>Mail check to:</span><br>
-            <b>Expanding Your Horizons</b><br>
-            <b>Texas Wesleyan University</b><br>
-            <b>1201 Wesleyan St.</b><br>
-            <b>Fort Worth, TX 76105</b>
-          </div>
-<footer class="footer mt-auto py-3 bg-primary">
-  <div class="container">
-    <span class="text-muted">© 2021 Texas Wesleyan Univeristy. All rights reserved..</span>
-  </div>
-</footer>
+<div class="text-center">
+  <span>Make checks payable to <u>Expanding Your Horizons</u>.</span><br>
+  <span>Mail check to:</span><br>
+  <b>Expanding Your Horizons</b><br>
+  <b>Texas Wesleyan University</b><br>
+  <b>1201 Wesleyan St.</b><br>
+  <b>Fort Worth, TX 76105</b>
+</div>
 
+<div id="footer">
+  <hr class="hr">
+  <a href="http://www.txwes.edu" target="_new"><img src="include/logo-twu.png" height="39" alt="Texas Wesleyan"></a>
+  © <a href="http://cs.txwes.edu" target="_new">
+    <script>
+      document.write((new Date()).getFullYear())
+    </script> The Computer Science Program
+  </a> <a href="http://cs.txwes.edu" target="_new"> at Texas Wesleyan University, Fort Worth, Texas</a>
+</div>
