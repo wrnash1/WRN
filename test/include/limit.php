fd7ae2b36c383620602096345d15a03e889e3541
Bill Nash <wrnash1@gmail.com>
2021-01-06 22:14:31 -0600

    m

--- test/include/limit.php
+++ test/include/limit.php
@@ -1,16 +1,6 @@
 <!-- MAIN DIV START -->
-<<<<<<< ours
 
 <div id="main-content">
   <h1>Student Registration</h1>
   <p>Space is limited, so register early! Registration confirmation will be emailed to your email address as soon as we've processed your information.</p>
   <p style="margin-top:-1em;"> * $5 registration fee mailed to TWU</p>
-=======
-<div class="container main-content">
-  <div class="row">
-    <h1>Student Registration</h1>
-    <p>Space is limited, so register early! Registration confirmation will be emailed to your email address as soon as we've processed your information.</p>
-    <p style="margin-top:-1em;"> * $5 registration fee mailed to TWU</p>
-  </div>
-</div>
->>>>>>> theirs
