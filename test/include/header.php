7a5b0e8acc240a7e2c8bdccdf5e278f2143993dd
Bill Nash <wrnash1@gmail.com>
2021-01-06 21:39:58 -0600

    merge

--- test/include/header.php
+++ test/include/header.php
@@ -1,12 +1,25 @@
 <!-- HEADER DIV START -->
+<<<<<<< ours
 <div id="header" class="container-fluid">
   <div class="row">
     <div class="col-10">
       <img id="logo1" src="https://txwes.edu/media/communications-office/brand-toolbox/txwes-logos/TxWesPrimaryLogo_onWhite.png" alt="logo">
     </div>
     <div class="col-2">
+=======
+<div class="header container-fluid">
+  <div class="row">
+    <div class="col-8">
+      <img id="logo1" src="https://txwes.edu/media/communications-office/brand-toolbox/txwes-logos/TxWesPrimaryLogo_onWhite.png" alt="logo">
+    </div>
+    <div class="col-4 ml-auto">
+>>>>>>> theirs
       <img id="logo2" src="static/eyh.png" alt="logo">
     </div>
   </div>
 </div>
+<<<<<<< ours
   <!-- HEADER DIV END -->
+=======
+<!-- HEADER DIV END -->
+>>>>>>> theirs
--- test/include/submit.php
+++ test/include/submit.php
@@ -1,3 +1,10 @@
-<div class="d-flex justify-content-center">
-  <button class="btn btn-primary" type="submit">Submit your Registration</button>
+<div class="container text-white">
+  <div class="row">
+    <div class="text-center">
+      <button class="btn btn-primary" type="submit">
+        Submit your Registration
+      </button>
+    </div>
+  </div>
 </div>
+<hr />
